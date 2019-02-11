bd09398f7019a893d4a2eb771137b8e1acfd6b7a
Taylon Kalel <taylon.kalel@gmail.com>
2019-02-11 21:20:03 -0200

    Problema1 - Nota ponderada

---
+++ Problema1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Teste
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite a primeira nota: ");
+            float n1 = float.Parse(Console.ReadLine());
+            Console.Write("Digite a segunda nota: ");
+            float n2 = float.Parse(Console.ReadLine());
+            Console.Write("Digite a terceira nota: ");
+            float n3 = float.Parse(Console.ReadLine());
+
+            float result = n1 * 0.1f + n2 * 0.3f + n3 * 0.6f;
+
+            Console.WriteLine("A média é: " + result);
+        }
+    }
+}
